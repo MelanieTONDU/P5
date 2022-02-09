976423e9c33b7469f9ad392e03bec5f1604a2a5c
Melanie TONDU <melanietondu.2@gmail.com>
2022-02-09 11:13:51 +0100

    Ajout fichier

---
+++ back/product.js
@@ -0,0 +1,87 @@
+// Récupération de l'id du produit grâce l'url de la page//
+let str = "http://localhost:3000/api/products.html" + window.location.href;
+let url = new URL(str);
+let id = url.searchParams.get("id");
+
+// Récupération et intégration des données du produit //
+fetch ("http://localhost:3000/api/products/" + id)
+.then(response => {
+  response.json().then(product => {
+    let description = document.getElementById('description');
+    description.innerHTML = '<p id="description"> '+product.description+' </p>';     
+      
+    let name = document.getElementById('title');
+    name.innerHTML ='<h1 id="title">'+product.name+'</h1>';
+    
+    let price = document.getElementById('price');
+    price.innerHTML= '<span id="price">'+product.price+'</span>';
+    
+    let image = document.querySelector(".item__img");
+    image.innerHTML= '<div class="item__img"><img src="'+product.imageUrl+'" alt="Photographie dun canapé"></div>';
+
+    for (let i=0; i<product.colors.length; i++) {
+      let newColor = document.createElement("option");
+      let color = document.getElementById("colors");
+      newColor.setAttribute('valeur', color[i]);
+      newColor.innerHTML = product.colors[i];
+      color.appendChild(newColor);
+    }
+  })
+})
+
+// Récupération des choix de l'utilisateur //
+const addColor = document.getElementById('colors');
+addColor.value;
+
+const addQuantity = document.getElementById('quantity');
+addQuantity.addEventListener('click', function(e){
+  e.preventDefault();
+  addQuantity.value;
+})
+
+//------------------- Au clic sur "Ajouter au panier" ---------------------//
+const addToCart = document.getElementById('addToCart');
+addToCart.addEventListener('click', function(e){
+  e.preventDefault();
+  if (addQuantity.value < 1 || addColor.value == ""){
+    alert("Oups ! Tu as oublié quelque chose ");
+  }
+  else if (addQuantity.value > 100){
+    alert("As-tu vraiment besoin d'autant de canapé ?");
+  }
+  else {
+
+  // Variable produit //
+    let optionProduct = {
+      productId : id,
+      optionColor : addColor.value,
+      optionQuantity : addQuantity.value,
+    }
+
+  // Local Storage //
+  let productStorage = JSON.parse(localStorage.getItem("product"));
+  
+    const ChangeQuantity = () => {
+      let foundProductId = productStorage.find(p => p.productId == optionProduct.productId);
+      if(foundProductId != undefined ){
+        let foundQuantity = parseInt(foundProductId.optionQuantity);
+        let foundOptionQuantity = parseInt(optionProduct.optionQuantity);
+        foundProductId.optionQuantity = foundQuantity + foundOptionQuantity;
+      }
+      else{
+        productStorage.push(optionProduct);
+      }
+    }
+
+    if(productStorage){
+      ChangeQuantity();
+      localStorage.setItem("product", JSON.stringify(productStorage));
+    }
+    else{
+      productStorage = [];
+      productStorage.push(optionProduct);
+      localStorage.setItem("product", JSON.stringify(productStorage));
+    }
+  }
+})
+//-----------------------------------------------------------//  
---
+++ back/script.js
@@ -0,0 +1,11 @@
+ fetch ("http://localhost:3000/api/products")
+  .then(response => {
+    response.json().then(products => {
+      let html = '';
+      products.forEach(product => {
+        html += '<a href="./product.html?id='+product._id+'"><article><img src="'+product.imageUrl+'" alt="Lorem ipsum dolor sit amet, Kanap name1"><h3 class="productName">'+ product.name +'</h3><p class="productDescription">'+product.description+'</p></article></a>'
+document.getElementById('items').innerHTML = html;
+       });
+    })
+  })
+    
